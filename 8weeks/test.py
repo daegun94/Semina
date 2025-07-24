7a0d78c98c2cc1f6f26e8431328192820fc5771a
daegun <daegun-notebook@gun.local>
2025-07-24 14:53:48 +0900

    test

--- 8weeks/test.py
+++ 8weeks/test.py
@@ -2,13 +2,13 @@ import requests
 import time
 import threading
 
-TARGET_URL = "http://1.1.1.1/login"
-REQUESTS_PER_SECOND = 100
+TARGET_URL = "http://<public ip로 변경>/login"
+REQUESTS_PER_SECOND = 10000
 
 def send_request():
     try:
         # 필요 시 payload, headers 등 추가 가능
-        response = requests.post(TARGET_URL, timeout=2)
+        response = requests.get(TARGET_URL, timeout=2)
         print(f"[{response.status_code}] {response.text}")
     except requests.RequestException as e:
         print(f"[ERROR] {e}")
